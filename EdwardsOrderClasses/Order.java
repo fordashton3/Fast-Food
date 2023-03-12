ee97b5ac7b2ce56e7b5a68669e37e9ea3401b7b7
Ejz9 <114949726+Ejz9@users.noreply.github.com>
2023-03-12 12:28:04 -0500

    !

---
+++ EdwardsOrderClasses/Order.java
@@ -0,0 +1,84 @@
+package EdwardsOrderClasses;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+
+
+public class Order {
+    private int orderId;
+    private ArrayList<String> foods;
+    private ArrayList<Integer> quantities;
+    private ArrayList<Double> unitPrices;
+
+    public Order(int orderId, ArrayList<String> foods, ArrayList<Integer> quantities, ArrayList<Double> unitPrices) {
+        this.orderId = orderId;
+        this.foods = foods;
+        this.quantities = quantities;
+        this.unitPrices = unitPrices;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public ArrayList<String> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(ArrayList<String> foods) {
+        this.foods = foods;
+    }
+
+    public ArrayList<Integer> getQuantities() {
+        return quantities;
+    }
+
+    public void setQuantities(ArrayList<Integer> quantities) {
+        this.quantities = quantities;
+    }
+
+    public ArrayList<Double> getUnitPrices() {
+        return unitPrices;
+    }
+
+    public void setUnitPrices(ArrayList<Double> unitPrices) {
+        this.unitPrices = unitPrices;
+    }
+
+    public double calculateTotalAmount() {
+        double total = 0;
+        for (int i = 0; i < foods.size(); i++) {
+            total += quantities.get(i) * unitPrices.get(i);
+        }
+        return total;
+    }
+
+    public double calculateTaxAmount() {
+        double taxRate = 0.05;
+        return calculateTotalAmount() * taxRate;
+    }
+
+    public double calculateFinalBillAmount() {
+        return calculateTotalAmount() + calculateTaxAmount();
+    }
+
+    public void printInvoice() {
+        DecimalFormat df = new DecimalFormat("#.##");
+        System.out.println("Order ID: " + orderId);
+        System.out.println("------------------------------");
+        System.out.println("Food\tQuantity\tUnit Price");
+        System.out.println("------------------------------");
+        for (int i = 0; i < foods.size(); i++) {
+            System.out.println(foods.get(i) + "\t" + quantities.get(i) + "\t\t\t$" + unitPrices.get(i));
+        }
+        System.out.println("------------------------------");
+        System.out.println("Total Amount: $" + df.format(calculateTotalAmount()));
+        System.out.println("Tax Amount: $" + df.format(calculateTaxAmount()));
+        System.out.println("Final Bill Amount: $" + df.format(calculateFinalBillAmount()));
+        System.out.println("");
+    }
+}
---
+++ EdwardsOrderClasses/OrderTest.java
@@ -0,0 +1,111 @@
+package EdwardsOrderClasses;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class OrderTest {
+    public static void main(String[] args) {
+        try (Scanner input = new Scanner(System.in)) {
+            ArrayList<String> mainDishes = new ArrayList<>();
+            mainDishes.add("Chicken");
+            mainDishes.add("Beef");
+            ArrayList<Double> mainDishPrices = new ArrayList<>();
+            mainDishPrices.add(8.00);
+            mainDishPrices.add(10.00);
+
+            ArrayList<String> sides = new ArrayList<>();
+            sides.add("Fries");
+            sides.add("Chips");
+            ArrayList<Double> sidePrices = new ArrayList<>();
+            sidePrices.add(2.00);
+            sidePrices.add(2.50);
+
+            ArrayList<String> drinks = new ArrayList<>();
+            drinks.add("Water");
+            drinks.add("Soda");
+            ArrayList<Double> drinkPrices = new ArrayList<>();
+            drinkPrices.add(1.00);
+            drinkPrices.add(1.50);
+
+            ArrayList<Order> orders = new ArrayList<>();
+            int i = 0;
+            while (true) {
+                if (i > 0) {
+                    System.out.println("Would you like to place another order? (n) to stop  | Enter any other value to continue");
+                    String value = input.next();
+                    if (value.equalsIgnoreCase("n") || value.equalsIgnoreCase("no")) {
+                        input.nextLine();
+                        break;
+                    }
+                }
+                System.out.println("Order " + (i + 1));
+                System.out.println("------------------------------");
+
+                System.out.println("Main Dish Options:");
+                for (int j = 0; j < mainDishes.size(); j++) {
+                    System.out.println((j + 1) + ". " + mainDishes.get(j) + " ($" + mainDishPrices.get(j) + ")");
+                }
+                int mainDishChoice = input.nextInt();
+                input.nextLine(); // Consume newline character
+                String mainDish = mainDishes.get(mainDishChoice - 1);
+                double mainDishPrice = mainDishPrices.get(mainDishChoice - 1);
+                int quantity1 = getQuantity(input);
+
+                System.out.println("Side Options:");
+                for (int j = 0; j < sides.size(); j++) {
+                    System.out.println((j + 1) + ". " + sides.get(j) + " ($" + sidePrices.get(j) + ")");
+                }
+                int sideChoice = input.nextInt();
+                input.nextLine(); // Consume newline character
+                String side = sides.get(sideChoice - 1);
+                double sidePrice = sidePrices.get(sideChoice - 1);
+                int quantity2 = getQuantity(input);
+
+                System.out.println("Drink Options:");
+                for (int j = 0; j < drinks.size(); j++) {
+                    System.out.println((j + 1) + ". " + drinks.get(j) + " ($" + drinkPrices.get(j) + ")");
+                }
+                int drinkChoice = input.nextInt();
+                input.nextLine(); // Consume newline character
+                String drink = drinks.get(drinkChoice - 1);
+                double drinkPrice = drinkPrices.get(drinkChoice - 1);
+                int quantity3 = getQuantity(input);
+
+                ArrayList<String> foods = new ArrayList<>();
+                foods.add(mainDish);
+                foods.add(side);
+                foods.add(drink);
+
+                ArrayList<Integer> quantities = new ArrayList<>();
+                quantities.add(quantity1);
+                quantities.add(quantity2);
+                quantities.add(quantity3);
+
+                ArrayList<Double> unitPrices = new ArrayList<>();
+                unitPrices.add(mainDishPrice);
+                unitPrices.add(sidePrice);
+                unitPrices.add(drinkPrice);
+
+                Order order = new Order(i + 1, foods, quantities, unitPrices);
+                orders.add(order);
+                i++;
+            }
+            for (int j = 0; j < orders.size(); j++) {
+                orders.get(j).printInvoice();
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Incorrect Input");
+        } catch (IndexOutOfBoundsException e){
+            System.out.println("Value out of bounds");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static int getQuantity(Scanner input){
+        System.out.println("Enter quantity:");
+        int quantity = input.nextInt();
+        return quantity;
+    }
+}
