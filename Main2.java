971b13ddb6b55cbd2b045f343cc666280ed29969
Ejz9 <114949726+Ejz9@users.noreply.github.com>
2023-03-15 12:45:59 -0500

    !

--- Main2.java
+++ Main2.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
+public class Main2 {
     public static void main(String[] args) throws IOException {
         Scanner input = new Scanner(System.in);
         int id = 1;
